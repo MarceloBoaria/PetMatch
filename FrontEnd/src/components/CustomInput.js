7990c22d47802dbf75491ce6299cca057c535887
Marcelo Boaria <marceloboaria@gmail.com>
2023-05-31 20:19:56 -0300

    front end

---
+++ FrontEnd/src/components/CustomInput.js
@@ -0,0 +1,39 @@
+import { Text, TextInput, StyleSheet, View } from 'react-native';
+import React from 'react';
+
+
+const CustomInput = ({ value, setValue, placeholder, secureTextEntry }) => {
+
+    return (
+        <View style={styles.container}>
+            <TextInput
+                value={value}
+                onChangeText={setValue}
+                placeholder={placeholder}
+                style={styles.input}
+                secureTextEntry={secureTextEntry}
+            />
+        </View>
+    )
+
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        backgroundColor: 'lightgray',
+        width: '100%',
+        marginVertical: 5,
+        borderWidth: 1,
+        borderColor: '#e8e8e8',
+        borderRadius: 5,
+    },
+
+    input: {
+        padding: 15,
+        fontWeight: 'bold',
+    }
+
+})
+
+export default CustomInput;
